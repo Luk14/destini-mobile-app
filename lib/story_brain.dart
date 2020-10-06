7b6c214ae5d4d910ae65178be7ce90d2021ab51f
Luk14 <lukaszplayz121@gmail.com>
2020-10-06 11:19:51 +0100

    State is now updated on Button Press, Map has been added to Structure the Story

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -1,6 +1,7 @@
 import 'package:destini_challenge_starting/story.dart';
 
 class StoryBrain {
+  int _storyID = 0;
 
   List<Story> _storyData = [
     Story(
@@ -29,34 +30,38 @@ class StoryBrain {
         '')
   ];
 
-//TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
-
-  String getStory()
-  {
-    return _storyData.first.storyTitle;
+  String getStory() {
+    return _storyData[_storyID].storyTitle;
   }
 
-  String getPath1()
-  {
-    return _storyData.first.path1;
+  String getPath1() {
+    return _storyData[_storyID].path1;
   }
 
-  String getPath2()
-  {
-    return _storyData.first.path2;
+  String getPath2() {
+    return _storyData[_storyID].path2;
   }
 
 //TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
 
-//TODO: Step 16 - Create a property called storyNumber which starts with a value of 0. This will be used to track which story the user is currently viewing.
+  void nextStory(int number) {
+    Map<int, Map<int, int>> choices = {
+      0: {1: 2, 2: 1},
+      2: {1: 5, 2: 4},
+      1: {1: 2, 2: 3},
+    };
+    List<int> storyEndBranches = [3, 4, 5];
+    if (storyEndBranches.contains(_storyID)) {
+      reset();
+    } else {
+      _storyID = choices[_storyID][number];
+    }
+  }
 
-//TODO: Step 17 - Create a method called nextStory(), it should not have any outputs but it should have 1 input called choiceNumber which will be the choice number (int) made by the user.
-
-//TODO: Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
-
-//TODO: Step 21 - Using the story plan, update nextStory() to change the storyNumber depending on the choice made by the user. e.g. if choiceNumber was equal to 1 and the storyNumber is 0, the storyNumber should become 2.
-
-//TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
+  void reset()
+  {
+    _storyID = 0;
+  }
 
 //TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
 }
